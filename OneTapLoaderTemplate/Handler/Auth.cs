f64837283020cfb8fefc106a65b7574020c9c806
Woke <66000596+woke999@users.noreply.github.com>
2021-12-15 18:39:46 -0500

    Add files via upload

---
+++ OneTapLoaderTemplate/Handler/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneTapLoaderTemplate.Handler
+{
+    internal class Auth
+    {
+        //use this class to auth your users//
+    }
+}
